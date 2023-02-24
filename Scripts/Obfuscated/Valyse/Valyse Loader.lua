f952bd136829ad9e0114a2f200cf76db2f6ac2c9
Random-Nooby <125040564+Random-Nooby@users.noreply.github.com>
2023-02-24 21:38:04 +0700

    Update Valyse Loader.lua

--- Scripts/Obfuscated/Valyse/Valyse Loader.lua
+++ Scripts/Obfuscated/Valyse/Valyse Loader.lua
@@ -1,17 +1,16 @@
-local CoreGui = game:GetService("StarterGui")
-
-local GameTable = {
-  [537413528] = "https://raw.githubusercontent.com/Random-Nooby/Projects/main/Scripts/Obfuscated/Valyse/Scripts/Build%20A%20Boat%20For%20Treasure.lua"
+local Scripts = {
+  [537413528] = "https://raw.githubusercontent.com/Random-Nooby/Projects/main/Scripts/Obfuscated/Valyse/Scripts/Build%20A%20Boat%20For%20Treasure.lua",
+  [8568266872] = "https://raw.githubusercontent.com/Random-Nooby/Projects/main/Scripts/Obfuscated/Valyse/Scripts/Kill%20Monsters%20to%20Save%20Princess.lua"
 }
 
 
 
-if GameTable[game.PlaceId] == nil then
-    CoreGui:SetCore("SendNotification", {
+if Scripts[game.PlaceId] == nil then
+    game:GetService("StarterGui"):SetCore("SendNotification", {
         Title = "Unsupported Game :(";
         Text = ":(";
         Duration = 5;
     })
 else
-    loadstring(game:HttpGet(GameTable[game.PlaceId]))()
+    loadstring(game:HttpGet(Scripts[game.PlaceId]))()
 end
