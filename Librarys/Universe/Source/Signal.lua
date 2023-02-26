74531e4a64e584f80e5694ae02d7448a300950ac
Random-Nooby <125040564+Random-Nooby@users.noreply.github.com>
2023-02-26 11:46:56 +0700

    Update Signal.lua

--- Librarys/Universe/Source/Signal.lua
+++ Librarys/Universe/Source/Signal.lua
@@ -1,4 +1,3 @@
----- Taken from https://raw.githubusercontent.com/Quenty/NevermoreEngine/version2/Modules/Shared/Events/Signal.lua
 --- Lua-side duplication of the API of events on Roblox objects.
 -- Signals are needed for to ensure that for local events objects are passed by
 -- reference rather than by value where possible, as the BindableEvent objects
