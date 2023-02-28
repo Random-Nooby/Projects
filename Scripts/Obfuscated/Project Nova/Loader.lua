a771a18e4274cb5d6de8fcf0c29e91d94b825da0
Random-Nooby <125040564+Random-Nooby@users.noreply.github.com>
2023-02-28 17:18:40 +0700

    Update Loader.lua

--- Scripts/Obfuscated/Project Nova/Loader.lua
+++ Scripts/Obfuscated/Project Nova/Loader.lua
@@ -1,1 +1,16 @@
+local Games = {
+    [286090429] = "https://raw.githubusercontent.com/Random-Nooby/Projects/main/Scripts/Obfuscated/Project%20Nova/Scripts/Arsenal.lua"
+}
 
+
+
+if Games[game.PlaceId] == nil then
+            game:GetService("StarterGui"):SetCore("SendNotification", {
+            Title = "Unsupported Game...",
+            Text = ":(",
+            Duration = 5
+        })
+
+else
+    loadstring(game:HttpGet(GameTable[game.PlaceId]))()
+end
